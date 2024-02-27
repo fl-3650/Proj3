cb70c5caabe8495e25cbd0209ce0ae3f375d47e6
fl_3650 <repilovi@gmail.com>
2024-02-27 23:08:45 +0300

    Before last

--- app/src/main/java/com/example/proj3/MainActivity.java
+++ app/src/main/java/com/example/proj3/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.proj3;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import 	android.util.TypedValue;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,21 +13,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+//        // ETC
+//        setContentView(R.layout.activity_main);
+
+//        // TASK 3
 //        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-//
 //        TextView textView = new TextView(this);
 //        textView.setText("Hello Android");
 //        textView.setTextSize(26);
-//
 //        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
 //                (ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
 //        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
 //        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
 //        textView.setLayoutParams(layoutParams);
 //        constraintLayout.addView(textView);
+//        setContentView(constraintLayout);
 
-        setContentView(R.layout.second_layout);
-        TextView textView = findViewById(R.id.header);
-        textView.setText("Hello from Java");
+//        // TASK 6
+//        setContentView(R.layout.second_layout);
+//        TextView textView = findViewById(R.id.header);
+//        textView.setText("Hello from Java");
+
+//        // TASK 12
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+        TextView textView = new TextView(this);
+        textView.setBackgroundColor(0xFFE0E0E0);
+        textView.setText("Hello Text");
+        textView.setTextSize(30);
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
+                (ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(60, 50, 60, 50);
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        textView.setLayoutParams(layoutParams);
+        textView.setPadding(40, 40, 40, 40);
+        constraintLayout.addView(textView);
+        setContentView(constraintLayout);
+
     }
 }
